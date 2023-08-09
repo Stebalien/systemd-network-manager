bb02146c546eb5623c95acf7cd0cb2fa2022aecf
Steven Allen <steven@stebalien.com>
2023-08-08 17:39:21 -0700

    correctly implement delays

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::time::Duration;
 
 use anyhow::Result;
 use clap::Parser;
-use futures::StreamExt;
 use tokio::time;
+use tokio_stream::StreamExt;
 use zbus::zvariant::OwnedObjectPath;
 use zbus::Connection;
 
@@ -26,7 +26,8 @@ static CAPTIVE_TARGET: &str = "captive-portal.target";
 mod network_manager_iface;
 mod systemd_iface;
 
-const CONNECTIVITY_TIMEOUT: Duration = Duration::from_secs(10);
+const CONNECTIVITY_TIMEOUT: Duration = Duration::from_secs(4);
+const OFFLINE_DELAY: Duration = Duration::from_secs(2);
 
 #[derive(Parser)]
 struct Args {
@@ -34,18 +35,48 @@ struct Args {
     user: bool,
 }
 
+#[derive(Debug, Copy, Clone, Default)]
+enum ConnectivityState {
+    #[default]
+    Offline,
+    Online,
+    CaptivePortal,
+}
+
+impl TryFrom<u32> for ConnectivityState {
+    type Error = u32;
+
+    fn try_from(value: u32) -> std::result::Result<Self, Self::Error> {
+        use ConnectivityState::*;
+        match value {
+            nm_state::DISCONNECTED => Ok(Offline),
+            nm_state::CONNECTED_GLOBAL => Ok(Online),
+            nm_state::CONNECTED_SITE => Ok(CaptivePortal),
+            e => Err(e),
+        }
+    }
+}
+
 async fn handle_state_change(
     systemd: &systemd_iface::ManagerProxy<'_>,
-    state: u32,
-) -> Result<Option<OwnedObjectPath>> {
+    state: ConnectivityState,
+) -> Result<OwnedObjectPath> {
     Ok(match state {
-        nm_state::CONNECTED_GLOBAL => Some(systemd.start_unit(ONLINE_TARGET, "replace").await?),
-        nm_state::DISCONNECTED => Some(systemd.start_unit(OFFLINE_TARGET, "replace").await?),
-        nm_state::CONNECTED_SITE => {
-            time::sleep(CONNECTIVITY_TIMEOUT).await;
-            Some(systemd.start_unit(CAPTIVE_TARGET, "replace").await?)
+        ConnectivityState::Online => systemd.start_unit(ONLINE_TARGET, "replace").await?,
+        ConnectivityState::Offline => {
+            // We wait a few seconds before going offline to avoid network "blips" restarting
+            // things.
+            time::sleep(OFFLINE_DELAY).await;
+            systemd.start_unit(OFFLINE_TARGET, "replace").await?
         }
-        _ => None,
+        ConnectivityState::CaptivePortal => {
+            // We wait a few seconds before transitioning to the "captive portal" target to avoid
+            // opening a login page unnecessarily. Might be better to just make it less intrusive,
+            // but network blips can cause the same issues... so it's better to just have a little
+            // delay here.
+            time::sleep(CONNECTIVITY_TIMEOUT).await;
+            systemd.start_unit(CAPTIVE_TARGET, "replace").await?
+        }
     })
 }
 
@@ -62,14 +93,18 @@ pub async fn main() -> Result<()> {
     let systemd = systemd_iface::ManagerProxy::new(&sd_conn).await?;
     let nm = network_manager_iface::NetworkManagerProxy::new(&nm_conn).await?;
 
-    let mut signals = nm.receive_state_changed().await?;
-    let mut next_state = nm.state().await?;
+    // States, filtered to only the ones we care about.
+    let mut states = nm
+        .receive_state_changed()
+        .await?
+        .filter_map(|s| s.args().map(|s| s.state.try_into().ok()).transpose());
+    let mut next_state = nm.state().await?.try_into().unwrap_or_default();
     loop {
         let active_task = handle_state_change(&systemd, next_state);
         tokio::select! {
-            signal = signals.next() => match signal {
-                Some(signal) => {
-                    next_state = signal.args()?.state
+            state = states.next() => match state {
+                Some(state) => {
+                    next_state = state?;
                 },
                 None => return Ok(()),
             },
