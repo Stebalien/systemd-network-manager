b88a2ffff7bb762005853b0dbfae0390b5d87f86
Steven Allen <steven@stebalien.com>
2025-12-11 07:22:05 -0800

    Fix nix architecture list

--- flake.nix
+++ flake.nix
@@ -20,9 +20,10 @@
     { moduleWithSystem, ... }:
     {
       systems = [
-        "x86-linux"
+        "i686-linux"
         "x86_64-linux"
         "aarch64-linux"
+        "armv7l-linux"
       ];
       imports = [ flake-parts.flakeModules.easyOverlay ];
       perSystem = { config, system, pkgs, ...}:
